6e2d3994aa6e053fa6106a8c6e30f21ba216fbd0
YA <YA-androidapp@users.noreply.github.com>
2019-12-28 14:46:55 +0900

    .env

--- Nuxtjs-GraphApi/pages/index.vue
+++ Nuxtjs-GraphApi/pages/index.vue
@@ -9,11 +9,7 @@
         My doozie Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
           Documentation
         </a>
         <a
@@ -29,13 +25,16 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
 
 export default {
   components: {
     Logo
+  },
+  created() {
+    console.log(process.env.GRAPH_CLIENT_ID);
   }
-}
+};
 </script>
 
 <style>
@@ -49,8 +48,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
