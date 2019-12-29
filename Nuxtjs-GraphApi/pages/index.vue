2828763baa59bb8ebf4c15b582d10568ee6a216e
YA <YA-androidapp@users.noreply.github.com>
2019-12-29 10:19:08 +0900

    Update index.vue

--- Nuxtjs-GraphApi/pages/index.vue
+++ Nuxtjs-GraphApi/pages/index.vue
@@ -1,13 +1,90 @@
 <template>
   <div class="container">
     <div>
-      <logo />
+      <div>
+        <b-navbar class="mb-3" toggleable="lg" type="dark" variant="dark">
+          <b-navbar-brand href="#">Nuxtjs-GraphApi</b-navbar-brand>
+
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+              <b-nav-item href="#">Link</b-nav-item>
+              <b-nav-item href="#" disabled>Disabled</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+              <b-nav-form>
+                <b-form-input
+                  size="sm"
+                  class="mr-sm-2"
+                  placeholder="Search"
+                ></b-form-input>
+                <b-button size="sm" class="my-2 my-sm-0" type="submit"
+                  >Search</b-button
+                >
+              </b-nav-form>
+
+              <b-nav-item-dropdown text="Lang" right>
+                <b-dropdown-item href="#">EN</b-dropdown-item>
+                <b-dropdown-item href="#">ES</b-dropdown-item>
+                <b-dropdown-item href="#">RU</b-dropdown-item>
+                <b-dropdown-item href="#">FA</b-dropdown-item>
+              </b-nav-item-dropdown>
+
+              <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template v-slot:button-content>
+                  <em>User</em>
+                </template>
+                <b-dropdown-item href="#">Profile</b-dropdown-item>
+                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-navbar>
+      </div>
+      <div>
+        <b-button v-b-toggle.collapse-1>Toggle Collapse</b-button>
+        <b-collapse id="collapse-1" class="mt-2">
+          <b-card>
+            <b-jumbotron
+              header="BootstrapVue"
+              lead="Bootstrap v4 Components for Vue.js 2"
+            >
+              <logo />
+              <p>For more information visit website</p>
+              <b-button variant="primary" href="#">More Info</b-button>
+            </b-jumbotron>
+          </b-card>
+        </b-collapse>
+      </div>
       <h1 class="title">
         Nuxtjs-GraphApi
       </h1>
       <h2 class="subtitle">
         My doozie Nuxt.js project
       </h2>
+      <div>
+        <div v-if="user">
+          <div>Welcome {{ user.name }}</div>
+          <div v-if="user.profile.jobTitle">
+            Your job title is {{ user.profile.jobTitle }}
+          </div>
+          <div>
+            <b-button variant="outline-primary" @click="$msal.signOut()"
+              >Sign out</b-button
+            >
+          </div>
+        </div>
+        <div v-else>
+          Please sign-in
+          <b-button variant="outline-primary" @click="$msal.signIn()"
+            >Sign in</b-button
+          >
+        </div>
+      </div>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">
           Documentation
@@ -26,19 +103,37 @@
 
 <script>
 import Logo from "~/components/Logo.vue";
+import { msalMixin } from "vue-msal";
 
 export default {
   components: {
     Logo
   },
+  computed: {
+    user() {
+      let user = null;
+      if (this.msal.isAuthenticated) {
+        // Note that the dollar sign ($) is missing from this.msal
+        user = {
+          ...this.msal.user,
+          profile: {}
+        };
+        if (this.msal.graph && this.msal.graph.profile) {
+          user.profile = this.msal.graph.profile;
+        }
+      }
+      return user;
+    }
+  },
   created() {
     console.log(process.env.GRAPH_CLIENT_ID);
     if (process.client) {
-      // if (!this.$msal.isAuthenticated()) {
-      this.$msal.signIn();
-      // }
+      if (!this.$msal.isAuthenticated()) {
+        this.$msal.signIn();
+      }
     }
-  }
+  },
+  mixins: [msalMixin]
 };
 </script>
 
